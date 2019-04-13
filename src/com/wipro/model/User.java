3a485aa7f70fc545ef43b04b96871f1794ce3676
leela <leelaprasadfine@gmail.com>
2019-04-13 11:55:02 +0530

    Added constructors

--- src/com/wipro/model/User.java
+++ src/com/wipro/model/User.java
@@ -3,6 +3,16 @@ package com.wipro.model;
 public class User {
 	private String name;
 	private int age;
+	
+	public User() {
+		super();
+	}
+	
+	public User(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
 
 	public String getName() {
 		return name;
